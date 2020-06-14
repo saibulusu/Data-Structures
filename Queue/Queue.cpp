1cfd891bc41ac734dcbde723193de2ef32ecdd7c
saibulusu <sai.bulusu@gmail.com>
2020-06-13 18:44:25 -0700

    Queue

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -31,3 +31,46 @@ template <typename T>
 int Queue<T>::getSize() {
     return size;
 }
+
+template <typename T>
+T Queue<T>::peek() {
+    if (size == 0) {
+        throw std::runtime_error("Peeking out of bounds");
+    } else {
+        return array[0];
+    }
+}
+
+template <typename T>
+void Queue<T>::enqueue(T key) {
+    array = (T*)realloc(array, (size + 1) * sizeof(T));
+    array[size] = key;
+    ++size;
+}
+
+template <typename T>
+T Queue<T>::dequeue() {
+    if (size == 0) {
+        throw std::runtime_error("Dequeuing out of bounds");
+    } else {
+        T key = array[0];
+        T* temp;
+        temp = (T*)malloc((size - 1) * sizeof(T));
+        for (int i = 0; i < size - 1; ++i) {
+            temp[i] = array[i + 1];
+        }
+        delete array;
+        array = temp;
+        temp = nullptr;
+        --size;
+        return key;
+    }
+}
+
+template <typename T>
+void Queue<T>::print() {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
--- Queue/Test.cpp
+++ Queue/Test.cpp
@@ -28,6 +28,101 @@ TEST_F(QueueTest, EmptySize){
     }
 }
 
+/** Check the first element from an empty queue */
+TEST_F(QueueTest, EmptyPeek) {
+    try {
+        int val = queue->peek();
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Peeking out of bounds");
+    }
+}
+
+/** Remove an element from an empty queue */
+TEST_F(QueueTest, EmptyDequeue) {
+    try {
+        int doesnotexist = queue->dequeue();
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Dequeuing out of bounds");
+    }
+}
+
+/** Enqueue and Dequeue once each and check the size is 0 */
+TEST_F(QueueTest, EnqueueDequeueSize) {
+    try {
+        queue->enqueue(3);
+        int three = queue->dequeue();
+        ASSERT_EQ(queue->getSize(), 0);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Check the value after enqueueing and dequeueing once each */
+TEST_F(QueueTest, EnqueueDequeueValue) {
+    try {
+        queue->enqueue(190);
+        int val = queue->dequeue();
+        ASSERT_EQ(val, 190);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Check the value after enqueueing multiple times and then deuqueing once */
+TEST_F(QueueTest, MultiEnqueueOneDequeue) {
+    try {
+        queue->enqueue(-1);
+        queue->enqueue(-2);
+        queue->enqueue(-4);
+        int val = queue->dequeue();
+        ASSERT_EQ(val, -1);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Enqueue multiple times then dequeue multiples times and check the size */
+TEST_F(QueueTest, MultiEnqueueMultiDequeueSize) {
+    try {
+        queue->enqueue(-1);
+        queue->enqueue(-4);
+        queue->enqueue(1000);
+        queue->enqueue(432);
+        queue->dequeue();
+        queue->dequeue();
+        queue->dequeue();
+        int size = queue->getSize();
+        ASSERT_EQ(size, 1);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Enqueue once and then peek */
+TEST_F(QueueTest, EnqueueOncePeek) {
+    try {
+        queue->enqueue(2);
+        int val = queue->peek();
+        ASSERT_EQ(val, 2);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Check that peek does not change the size of the queue */
+TEST_F(QueueTest, EnqueueOncePeekSize) {
+    try {
+        queue->enqueue(10);
+        int val = queue->peek();
+        int size = queue->getSize();
+        ASSERT_EQ(size, 1);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
