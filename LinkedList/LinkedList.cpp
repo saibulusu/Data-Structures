f9baf4cb67014e923d45fa179b6e9af2165d1f21
saibulusu <sai.bulusu@gmail.com>
2020-06-13 20:31:17 -0700

    Working on linked list

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -2,7 +2,7 @@
 
 template <typename T>
 struct ListNode {
-    ListNode* next;
+    ListNode<T>* next;
     T data;
 };
 
@@ -29,8 +29,10 @@ public:
 template <typename T>
 LinkedList<T>::LinkedList() {
     size = 0;
+    head = tail = nullptr;
 }
 
+/** Destruct the LinkedList */
 template <typename T>
 LinkedList<T>::~LinkedList() {
     ListNode<T>* cur = head;
@@ -39,4 +41,97 @@ LinkedList<T>::~LinkedList() {
         delete cur;
         cur = curNext;
     }
+}
+
+/** Get a value in the linked list at a certain index */
+template <typename T>
+T LinkedList<T>::get(int index) {
+    if (index >= size || index < 0) {
+        throw std::runtime_error("Getting out of bounds");
+    }
+    ListNode<T>* cur = head;
+    while (index > 0) {
+        cur = cur->next;
+        --index;
+    }
+    return cur->data;
+}
+
+/** Insert a value into the linked list at the end */
+template <typename T>
+void LinkedList<T>::insert(T key) {
+    if (size == 0) {
+        head = tail = new ListNode<T>;
+        tail->next = nullptr;
+        ++size;
+        return;
+    }
+    tail->next = new ListNode<T>;    
+    tail->next->data = key;
+    tail->next->next = nullptr;
+    tail = tail->next;
+    ++size;
+}
+
+/** Insert a value into the linked list at a certain index */
+template <typename T>
+void LinkedList<T>::insert(int index, T key) {
+    if (index > size + 1) {
+        throw std::runtime_error("Inserting out of bounds");
+    }
+    if (index == size) {
+        insert(key);
+    }
+}
+
+/** Alter a value in the linked list at a certain index */
+template <typename T>
+T LinkedList<T>::alter(int index, T key) {
+    if (index > size || index < 0) {
+        throw std::runtime_error("Altering out of bounds");
+    }
+}
+
+/** Remove a value in the linked list at a certain index */
+template <typename T>
+T LinkedList<T>::remove(int index) {
+    if (index > size || index < 0) {
+        throw std::runtime_error("Removing out of bounds");
+    }
+    ListNode<T>* rem = head;
+    if (index == 0) {
+        T val = head->data;
+        head = head->next;
+        delete rem;
+        rem = nullptr;
+        --size;
+        return val;
+    }
+    while (index > 1) {
+        rem = rem->next;
+        --index;
+    }
+    ListNode<T>* temp = rem->next;
+    rem->next = temp->next;
+    T val = temp->data;
+    delete temp;
+    --size;
+    return val;
+}
+
+/** Access the size of the linked list */
+template <typename T>
+int LinkedList<T>::getSize() {
+    return size;
+}
+
+/** Print the elements in the linked list */
+template <typename T>
+void LinkedList<T>::print() {
+    ListNode<T>* cur = head;
+    while (cur != nullptr) {
+        std::cout << cur->data << " ";
+        cur = cur->next;
+    }
+    std::cout << std::endl;
 }
--- LinkedList/Test.cpp
+++ LinkedList/Test.cpp
@@ -9,15 +9,149 @@
 class LinkedListTest : public ::testing::Test {
     protected:
     virtual void SetUp() {
-        array = new LinkedList<int>;
+        list = new LinkedList<int>;
     }
 
     virtual void TearDown() {
-        delete array;
+        delete list;
     }
 
-    LinkedList<int>* array;
+    LinkedList<int>* list;
 };
+
+/** Check the size of an empty list */
+TEST_F(LinkedListTest, SizeNoElements) {
+    try {
+        int size = list->getSize();
+        ASSERT_EQ(size, 0);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Check the size after inserting a few values */
+TEST_F(LinkedListTest, InsertFewSize) {
+    try {
+        list->insert(0);
+        list->insert(-1);
+        list->insert(-100);
+        list->insert(200);
+        int size = list->getSize();
+        ASSERT_EQ(size, 4);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Insert a few values and then check the value at the end */
+TEST_F(LinkedListTest, InsertCheckValue) {
+    try {
+        list->insert(0);
+        list->insert(-1);
+        list->insert(10);
+        list->insert(4);
+        int val = list->get(3);
+        ASSERT_EQ(val, 4);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Get an element from an empty list */
+TEST_F(LinkedListTest, GetEmpty) {
+    try {
+        int doesnotexist = list->get(0);
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Getting out of bounds");
+    }
+}
+
+/** Alter an empty list */
+TEST_F(LinkedListTest, AlterEmpty) {
+    try {
+        int doesnotexist = list->alter(2, 100);
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Altering out of bounds");
+    }
+}
+
+/** Alter with a negative index */
+TEST_F(LinkedListTest, AlterNegative) {
+    try {
+        int doesnotexist = list->alter(-2, 100);
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Altering out of bounds");
+    }
+}
+
+/** Removing from an empty list */
+TEST_F(LinkedListTest, RemoveEmpty) {
+    try {
+        int doesnotexist = list->remove(2);
+        FAIL();
+    } catch (std::exception& e) {
+        ASSERT_STREQ(e.what(), "Removing out of bounds");
+    }
+}
+
+/** Insert and remove in alternate fashion then check size */
+TEST_F(LinkedListTest, InsertRemoveAlternateSize) {
+    try {
+        list->insert(0);
+        int a = list->remove(0);
+        list->insert(100);
+        int b = list->remove(0);
+        list->insert(99);
+        int size = list->getSize();
+        ASSERT_EQ(size, 1);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Insert and remove in alternate fashion then check any value */
+TEST_F(LinkedListTest, InsertRemoveAlternateValue) {
+    try {
+        list->insert(-100);
+        list->print();
+        int a = list->remove(0);
+        list->insert(3030);
+        list->insert(-1000);
+        int b = list->remove(1);
+        int val = list->get(0);
+        
+        ASSERT_EQ(val, 3030);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Insert at 0 to an empty list */
+TEST_F(LinkedListTest, InsertAtZeroEmptySize) {
+    try {
+        list->insert(0, 100);
+        int size = list->getSize();
+        ASSERT_EQ(size, 1);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
+
+/** Insert a few values then check inserting at an index */
+TEST_F(LinkedListTest, InsertFewInsertAtIndexValue) {
+    try {
+        list->insert(0, 100);
+        list->insert(0, 200);
+        list->insert(0, 300);
+        int val = list->get(2);
+        ASSERT_EQ(val, 100);
+    } catch (std::exception& e) {
+        FAIL();
+    }
+}
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
